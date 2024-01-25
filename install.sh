c2678691dd5f639a9ac4e9e72ea96626a6b8c164
wheatbread2056 <geometrydashiscool222@gmail.com>
2024-01-25 14:09:49 -0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # this is currently broken for some reason
-sudo rm -rf /usr/src/bsh
-sudo cp -r ./ /usr/src/bsh
-sudo chmod +x /usr/src/bsh/breadshell.py
-sudo ln /usr/src/bsh/breadshell.py /bin/breadshell
+sudo rm -rf /usr/src/breadshell
+sudo cp -r ./ /usr/src/breadshell
+sudo chmod +x /usr/src/breadshell/breadshell.py
+sudo ln /usr/src/breadshell/breadshell.py /bin/breadshell
 echo breadshell successfully installed
