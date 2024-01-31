b81732e1a26c00a0c9383cbd7024721ddfa10647
wheatbread2056 <geometrydashiscool222@gmail.com>
2024-01-30 23:15:50 -0800

    add colortester (0.4-dev2)

--- breadshell.py
+++ breadshell.py
@@ -1,6 +1,8 @@
 # os library is NEEDED for this to run
 import os
 
+# IMPORT ALL DEPENDENCIES
+
 # import time
 try:
     import time
@@ -16,6 +18,15 @@ except:
     print('datetime not installed, installing now')
     os.system('sudo apt install python3-datetime -y')
     import datetime
+
+# import random
+try:
+    import random
+except:
+    print('random not installed, installing now')
+    os.system('sudo apt install python3-random -y')
+    import random
+
 # import colorama
 try:
     import colorama
@@ -54,6 +65,8 @@ class c:
     blue = colorama.Fore.BLUE
     magenta = colorama.Fore.MAGENTA
     cyan = colorama.Fore.CYAN
+    white = colorama.Fore.WHITE
+    black = colorama.Fore.BLACK
     r = colorama.Fore.RESET # resets color to default
 # background colors
 class bc:
@@ -63,6 +76,8 @@ class bc:
     blue = colorama.Back.BLUE
     magenta = colorama.Back.MAGENTA
     cyan = colorama.Back.CYAN
+    white = colorama.Back.WHITE
+    black = colorama.Back.BLACK
     r = colorama.Back.RESET # resets color to default
 
 # basic functions
@@ -79,23 +94,85 @@ def fatalerror(msg='A fatal error has occured, exiting immediately'):
 # game scripts
 
 # utility scripts
-    
-def start_calculator():
+
+def startu_colortester():
+    # random colors+chars for 256 characters
+    chars = '`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?'
+    str = ''
+    for i in range(4096):
+        a = random.randint(0,len(chars)-1)
+        char = chars[a]
+        b = random.randint(1,9)
+
+        # skipping d since c is used for foreground colors
+        if b == 1:
+            d = c.red
+        elif b == 2:
+            d = c.yellow
+        elif b == 3:
+            d = c.green
+        elif b == 4:
+            d = c.blue
+        elif b == 5:
+            d = c.cyan
+        elif b == 6:
+            d = c.magenta
+        elif b == 7:
+            d = c.white
+        elif b == 8:
+            d = c.black
+        elif b == 9:
+            d = c.r
+
+        # reroll colors to be used for background
+        b = random.randint(1,9)
+
+        if b == 1:
+            e = bc.red
+        elif b == 2:
+            e = bc.yellow
+        elif b == 3:
+            e = bc.green
+        elif b == 4:
+            e = bc.blue
+        elif b == 5:
+            e = bc.cyan
+        elif b == 6:
+            e = bc.magenta
+        elif b == 7:
+            e = bc.white
+        elif b == 8:
+            e = bc.black
+        elif b == 9:
+            e = bc.r
+
+        # add it to the main string
+        str=str+d+e+char
+
+    # +c.r+bc.r is needed to reset the colors
+    print(str+c.r+bc.r)
+
+def startu_calculator():
     while True:
+        # command (meant to be an expression)
         cmd = input(f'{c.cyan}calculator{c.r} > ')
         if cmd == 'exit':
             utillauncher()
+        # funny easter egg
         elif cmd == '9+10':
             print(21)
         else:
             try:
+                # print() is needed because simply executing 1+1 does not display 2
                 exec(f'print({cmd})')
             except:
                 throwerror()
 
-def start_python():
+def startu_python():
     while True:
+        # command
         cmd = input(f'{c.cyan}python{c.r} > ')
+
         if cmd == 'exit':
             utillauncher()
         else:
@@ -110,38 +187,48 @@ def games():
     globalversion = '0.2'
     # list amount of games here
     games = ['GAMES COMING SOON']
+
     print(f'bread games version {globalversion}')
     print(f'please select the game you would like to start ({len(games)} found):')
+
     i = 1
     for game in games:
         print(f'{c.yellow}{i} - {game}')
         i += 1
+    
     print(f'{c.yellow}exit - exit')
+
     game = input(f'{c.cyan}breadgames{c.r} > ')
+
     if game == 'exit':
         main()
     if games[int(game)-1] in games:
         print(f'Loading {games[int(game)-1]}...')
-        exec(f'start_{games[int(game)-1]}()')
+        exec(f'startg_{games[int(game)-1]}()')
 
 # utility launcher (totally not just modified game launcher)
 def utillauncher():
     globalversion = '0.2'
     # list amount of games here
     utilities = ['colortester','calculator','python']
+
     print(f'bread utilities version {globalversion}')
     print(f'please select the utility you would like to start ({len(utilities)} found):')
+
     i = 1
     for utility in utilities:
         print(f'{c.yellow}{i} - {utility}')
         i += 1
+
     print(f'{c.yellow}exit - exit')
+
     utility = input(f'{c.cyan}breadutils{c.r} > ')
+
     if utility == 'exit':
         main()
     if utilities[int(utility)-1] in utilities:
         print(f'Loading {utilities[int(utility)-1]}...')
-        exec(f'start_{utilities[int(utility)-1]}()')
+        exec(f'startu_{utilities[int(utility)-1]}()')
 
 # start of program, shown when opening the file
 print(f'version {c.cyan}{version}{c.r}, latest login {c.magenta}{datetime.datetime.now()}{c.r}')
