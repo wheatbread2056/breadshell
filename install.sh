e1be3de640f881ed1a1e1fefab05927eacb5483b
wheatbread2056 <geometrydashiscool222@gmail.com>
2024-01-31 18:31:56 -0800

    add option to make breadshell the default shell

--- install.sh
+++ install.sh
@@ -7,4 +7,11 @@ sudo cp -r ./ /usr/src/breadshell/
 sudo chmod +x /usr/src/breadshell/launch.sh
 # make the link so breadshell can be used as a command
 sudo ln /usr/src/breadshell/launch.sh /bin/breadshell
-echo breadshell successfully installed
+echo 'would you like to make breadshell the default shell? (Y/n)'
+read choice
+if ['$choice' = 'Y'] then
+    echo '/bin/breadshell' | sudo tee -a /etc/shells
+    chsh -s /bin/breadshell
+    echo breadshell is now the default shell, reboot to apply changes
+else
+    echo breadshell successfully installed
