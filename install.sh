359d4ac4fcb553267a9d06da9567efae6771777c
wheatbread2056 <geometrydashiscool222@gmail.com>
2024-01-31 18:55:22 -0800

    fix conditional statement in install.sh for the last time

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ sudo chmod +x /usr/src/breadshell/launch.sh
 sudo ln /usr/src/breadshell/launch.sh /bin/breadshell
 echo 'would you like to make breadshell the default shell? (Y/n)'
 read choice
-if [ '$choice' = 'Y' ]; then
-    echo '/bin/breadshell' | sudo tee -a /etc/shells
+if [ "$choice" = 'Y' ]; then
+    echo '/bin/breadshell' > /dev/null | sudo tee -a /etc/shells
     chsh -s /bin/breadshell
     echo breadshell is now the default shell, reboot to apply changes
 else
