d7a68529e08f1108141df840998ce5a7ac9bfe0e
wheatbread2056 <geometrydashiscool222@gmail.com>
2024-06-24 20:44:02 -1000

    /bin --> /sbin

--- install.sh
+++ install.sh
@@ -7,18 +7,19 @@ else
 fi
 # remove broken files from earlier installations
 sudo rm -rf /usr/src/breadshell
-sudo rm /bin/breadshell
+sudo rm /bin/breadshell # legacy (pre 1.0)
+sudo rm /sbin/breadshell
 # create the directory
 sudo cp -r ./ /usr/src/breadshell/
 # make launch.sh executable
 sudo chmod +x /usr/src/breadshell/launch.sh
 # make the link so breadshell can be used as a command
-sudo ln /usr/src/breadshell/launch.sh /bin/breadshell
+sudo ln /usr/src/breadshell/launch.sh /sbin/breadshell
 echo 'would you like to make breadshell the default shell? (Y/n)'
 read choice
 if [ "$choice" = 'Y' ]; then
-    echo '/bin/breadshell' > /dev/null | sudo tee -a /etc/shells
-    chsh -s /bin/breadshell
+    echo '/sbin/breadshell' > /dev/null | sudo tee -a /etc/shells
+    chsh -s /sbin/breadshell
     echo breadshell is now the default shell, reboot to apply changes
 else
     echo breadshell successfully installed
